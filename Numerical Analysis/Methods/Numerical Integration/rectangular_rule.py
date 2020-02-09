257496f36e91dc31dc62db9cda4a3bb944495076
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-02-09 11:41:25 +0500

    update

--- Numerical Analysis/Methods/Numerical Integration/rectangular_rule.py
+++ Numerical Analysis/Methods/Numerical Integration/rectangular_rule.py
@@ -1,34 +1,44 @@
+import tabulate as tb
 import numpy as np
 import matplotlib.pyplot as plt
 
 fx = lambda x: 1 / (1 + x ** 2)
 
-a = 0  # lower limit
-b = 5  # upper limit
-
-n = 10  # slices
-h = abs(b - a) / n  # width
+# lower, upper limits, slices and width
+l = 2
+u = 5
+n = 7
+w = abs(l - u) / n
 
 # plot function
-X = np.linspace(a, b, n)
-Y = fx(X)
-plt.plot(X, Y, color='orange')
+xPoints = np.linspace(l, u)
+yPoints = fx(xPoints)
+plt.plot(xPoints, yPoints, color='orange')
 
-XS = np.linspace(a, b, n + 1)
-YS = fx(XS)
-plt.plot(XS, YS, color='blue')
+# n no of x points
+XS = np.linspace(l, u, n + 1)
+
+ans = []
 
 area = 0
+
 for i in range(n):
-    # plot rectangle
-    plt.fill([XS[i], XS[i], XS[i + 1], XS[i + 1]],
-             [0, fx(XS[i]), fx(XS[i]), 0],
-             color='blue', edgecolor='black', alpha=0.5)
-    plt.pause(0.2)
+    # plot rectangular
+    print(XS[i], XS[i + 1])
+    plt.fill([XS[i], XS[i], XS[i + 1], XS[i + 1]], [0, fx(XS[i]), fx(XS[i]), 0], color='blue', edgecolor='black',
+             alpha=0.5)
+    plt.pause(0.002)
+
+    # append to ans array
+    ans.append([XS[i], 2 * fx(XS[i])])
 
     # sum integrated area
-    area += Y[i] * h
+    area += fx(XS[i])
 
+area = area * w
+
+print(tb.tabulate(ans, headers=['x', 'fx(x)'], tablefmt='orgtbl', showindex='always'))
 print('Integrated area: ', area)
+
 plt.title('Rectangular Rule, N = {}'.format(n))
 plt.show()
--- test.py
+++ test.py
@@ -1,30 +1,8 @@
-from tkinter import *
-from tkinter import messagebox
-win = Tk()
-win.title("Temprature calculator")
-def convert(args):
-    a=f.get()
-    at=0
-    at=int(a)
-    ad=((at)-32)*(5/9)
-    c.insert(0,str(ad))
 
 
-Label(win, text='Forenheit').grid(row=0)
-Label(win, text='Celsius').grid(row=1)
-f = Entry(win)
-c = Entry(win)
-f.grid(row=0, column=1)
-c.grid(row=1, column=1)
-
-fr = Label(win, text='...')
-fr.grid(row=0, column=3)
-cr = Label(win, text='...')
-cr.grid(row=1, column=3)
+a = 2
+b = 2
 
 
 
-
-Button(win, text='Convert', command=convert).grid(row=2)
-
-win.mainloop()
+print(a >= b)
