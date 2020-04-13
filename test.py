19a9d216511e42c375f94d34e2bfdfda53d0551d
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-04-13 20:37:55 +0500

    test

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-a = 2
-b = 2
+import numpy as np
+import matplotlib.pyplot as plt
 
+greyhounds = 500
+labs = 500
 
+grey_height = 28 + 4 * np.random.randn(greyhounds)
+lab_height = 24 + 4 * np.random.randn(labs)
 
-print(a >= b)
+plt.hist([grey_height, lab_height], stacked=True, color=['r', 'b'])
+plt.show()
